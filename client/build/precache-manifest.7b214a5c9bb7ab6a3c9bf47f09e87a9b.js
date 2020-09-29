3b9ba13103821fbd90bdeaea1faf612928cda7b0
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-09-29 10:40:11 +0200

    first commit

---
+++ client/build/precache-manifest.7b214a5c9bb7ab6a3c9bf47f09e87a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d604795442f83d8b31fd",
+    "url": "/static/css/main.d86097fe.chunk.css"
+  },
+  {
+    "revision": "d604795442f83d8b31fd",
+    "url": "/static/js/main.d6047954.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f80efb79c54430a10886",
+    "url": "/static/js/2.f80efb79.chunk.js"
+  },
+  {
+    "revision": "8e78b2308f8d5a2f9b69b4c751fb3bfd",
+    "url": "/index.html"
+  }
+];
